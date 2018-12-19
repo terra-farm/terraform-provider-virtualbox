830c354425a9b636fab9118153c4790259a34541
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2018-12-19 00:33:24 +0000

    Fix go linting problems.
    Fix the problems with Go styling, such as serious
    underscores_in_variables, as well as less pressing issues, but still
    following the Go's Code Review Comments wiki page.
    
    Side effect is that errors are actually tracable and a full error log
    message is visible.

--- virtualbox/provider.go
+++ virtualbox/provider.go
@@ -1,6 +1,9 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// Package virtualbox serves as an entrypoint, returning the list of available
+// resources for the plugin.
 package virtualbox
 
 import (
@@ -18,6 +21,7 @@ func init() {
 	log.SetPrefix(fmt.Sprintf("pid-%d-", os.Getpid()))
 }
 
+// Provider returns a resource provider for virtualbox.
 func Provider() terraform.ResourceProvider {
 	return &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
