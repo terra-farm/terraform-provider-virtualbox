7d34dd2a0dbbdee876cb00f76d83ea3c4812411a
Ringo De Smet <ringo@de-smet.name>
2019-08-01 23:14:30 +0200

    Added missing shell script to create a Github release.

---
+++ scripts/dist.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+which sha256sum
+
+mkdir dist
+OS="linux"
+case "${OSTYPE}" in
+  darwin*)  OS="darwin" ;; 
+  linux*)   OS="linux" ;;
+  *)        echo "unknown: ${OSTYPE}" ;;
+esac
+
+cp "${GOPATH}/bin/terraform-provider-xenserver" "dist/terraform-provider-xenserver-${TRAVIS_TAG}-${OS}_amd64"
+sha256sum "dist/terraform-provider-xenserver-${TRAVIS_TAG}-${OS}_amd64" | awk '{ print $1 }' > "dist/terraform-provider-xenserver-${TRAVIS_TAG}-${OS}_amd64.sha256sum"
+ls -lsa dist/
+cat "dist/terraform-provider-xenserver-${TRAVIS_TAG}-${OS}_amd64.sha256sum"
