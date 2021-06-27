d55c0d5752b9fb5d5122885fb0cbb86648efc276
jpts <jpts@users.noreply.github.com>
2021-06-27 23:33:02 +0100

    Update to use offical Terraform plugin schema

--- virtualbox/resource_natnetwork.go
+++ virtualbox/resource_natnetwork.go
@@ -4,7 +4,7 @@
 package virtualbox
 
 import (
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	vbox "github.com/terra-farm/go-virtualbox"
 )
 
